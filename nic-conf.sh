f5cd3b731b800b425f40eb6b52b591f5b1bb1f66
root <root@cool1.blr.asicdesigners.com>
2020-06-10 11:33:12 +0530

    NIC changes more

--- nic-conf.sh
+++ nic-conf.sh
@@ -3,6 +3,11 @@ cd /root/iproute2/tc
 ./tc filter add dev enp3s0f4 parent 1: protocol ip prio 1 u32 match ip dst 102.28.28.158 action skbedit queue_mapping 2
 cd /root/Sony/TM-scripts/
 
+To show filters 
+---------------
+tc -s filter show dev enp3s0f4
+
+
 
 For VLAN
 --------
